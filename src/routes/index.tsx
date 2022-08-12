7f079ba7a1bc6746d87ad73f2f0bdd6bba0d0370
xujie <jyoketsu@gmail.com>
2022-08-12 16:42:25 +0800

    style: 调色

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,21 +7,61 @@ import Login from "../pages/login";
 import { useMemo } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { PaletteMode } from "@mui/material";
+import { grey, red } from "@mui/material/colors";
 import { useAppSelector } from "../redux/hooks";
 const BASE = import.meta.env.VITE_BASE;
 
 export default function Router() {
-  const dark = useAppSelector((state) => state.common.dark);
+  const isDark = useAppSelector((state) => state.common.dark);
+
+  // 配色
+  // 默认颜色：https://mui.com/zh/material-ui/customization/default-theme/
+  // 调色：https://mui.com/zh/material-ui/customization/color/#picking-colors
+  const getDesignTokens = (mode: PaletteMode) => ({
+    palette: {
+      mode,
+      ...(mode === "light"
+        ? {
+            // palette values for light mode
+            primary: {
+              main: red[800],
+            },
+            background: {
+              default: grey[100],
+              paper: "#FFF",
+            },
+          }
+        : {
+            // palette values for dark mode
+            primary: {
+              main: red[700],
+            },
+            background: {
+              default: grey[900],
+              paper: grey[800],
+            },
+          }),
+    },
+  });
 
   const theme = useMemo(
     () =>
       createTheme({
-        palette: {
-          mode: dark ? "dark" : "light",
+        ...getDesignTokens(isDark ? "dark" : "light"),
+        breakpoints: {
+          values: {
+            xs: 0,
+            sm: 400,
+            md: 600,
+            lg: 900,
+            xl: 1200,
+          },
         },
       }),
-    [dark]
+    [isDark]
   );
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />

d0b8efe639f9184f68193ae29e1ef190aea50699
xujie <xujie@titanai.cc>
2025-12-30 14:49:05 +0800

    feat: 使用shadcn/ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "React Template" },
+    { name: "description", content: "Welcome to React Template!" },
   ];
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: { host: '0.0.0.0' }
 });
